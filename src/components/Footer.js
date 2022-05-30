692370da2a8239ff4e2ed0c3b3da3f3cad32ff1c
Vkar90 <kardamitsis.e@gmail.com>
2022-05-30 15:52:29 +0300

    add fire instructions modal

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,51 +1,54 @@
 import React from "react";
 import acropolLogo from "../assets/acropol-logo.jpeg";
+import Modal from "./Modal";
 
 const Footer = () => {
   return (
-    <footer class="p-4 bg-white rounded-lg shadow md:px-6 md:py-8 dark:bg-gray-800">
-      <div class="sm:flex sm:items-center sm:justify-center">
-        <a href="http://www.acropol.gr/" class="flex items-center mb-4 sm:mb-0">
+    <footer className="p-4 bg-white rounded-lg shadow md:px-6 md:py-8 dark:bg-gray-800">
+      <div className="sm:flex sm:items-center sm:justify-center">
+        <a
+          href="http://www.acropol.gr/"
+          className="flex items-center mb-4 sm:mb-0"
+        >
           <img
             src={acropolLogo}
-            class="mr-3 h-10"
+            className="mr-3 h-10"
             alt="Acropol Hotel Logo"
             width="40px"
             height="40px"
           />
         </a>
-        <ul class="flex flex-wrap items-center mb-6 text-sm text-gray-500 sm:mb-0 dark:text-gray-400">
+        <ul className="flex flex-wrap items-center mb-6 text-sm text-gray-500 sm:mb-0 dark:text-gray-400">
           <li>
-            <a href="#" class="mr-4 hover:underline md:mr-6 ">
-              Fire Safety
-            </a>
+            <Modal />
           </li>
           <li>
             <a
               href="https://acropol-menu.netlify.app/"
-              class="mr-4 hover:underline md:mr-6"
+              className="mr-4 hover:underline md:mr-6"
               target="_blank"
+              rel="noreferrer"
             >
               Restaurant Menu
             </a>
           </li>
           <li className="mr-3">+30 210 68 26 650</li>
           <li>
-            <a href="mailto: info@acropol.gr" class="hover:underline">
+            <a href="mailto: info@acropol.gr" className="hover:underline">
               info@acropol.gr
             </a>
           </li>
         </ul>
       </div>
-      <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-      <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
+      <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+      <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
         © 2022 Acropol Hotel™ . All Rights Reserved.
       </span>
-      <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
+      <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
         Designed & Developed by{" "}
         <a
           href="https://belowthefold.gr"
-          class=" text-red-700 font-bold hover:underline"
+          className=" text-red-700 font-bold hover:underline"
         >
           Below The Fold
         </a>
---
+++ src/components/Modal.js
@@ -0,0 +1,97 @@
+import React, { useState } from "react";
+
+const Modal = () => {
+  const [showModal, setShowModal] = useState(false);
+
+  return (
+    <>
+      <button
+        className="bg-red-700 text-white active:bg-pink-600 font-bold text-sm px-3 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+        type="button"
+        onClick={() => setShowModal(true)}
+      >
+        Fire Safety
+      </button>
+      {showModal ? (
+        <>
+          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+            <div className="relative w-auto my-6 mx-auto max-w-3xl">
+              {/*content*/}
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                {/*header*/}
+                <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                  <h3 className="text-3xl font-semibold">
+                    Fire Safety Instructions
+                  </h3>
+                  <button
+                    className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                    onClick={() => setShowModal(false)}
+                  >
+                    <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                      ×
+                    </span>
+                  </button>
+                </div>
+                {/*body*/}
+                <div className="relative p-6 flex-auto">
+                  <ul className="my-4 text-slate-500 text-lg leading-relaxed">
+                    <li>• Do not smoke in your room.</li>
+                    <li>• Always use the ashtray provided at the balcony.</li>
+                    <li>
+                      • Take note of the Emergency Exit Plan which is located at
+                      your room’s entrance
+                    </li>
+                    <li>
+                      • In case of disable people please inform the Reception.
+                    </li>
+                  </ul>
+                  <strong>What to do if you notice fire:</strong>
+                  <ul>
+                    <li>• Stay calm, do not panic.</li>
+                    <li>• Inform the Reception.</li>
+                    <li>
+                      • Raise the alarm by breaking the glass of the nearest
+                      “Fire Call Point”.
+                    </li>
+                    <li>• Evacuate the room closing all doors behind you.</li>
+                    <li>• Do not stop to collect personal belongings.</li>
+                    <li>
+                      • Alert floor room maid or any other member of the staff.
+                    </li>
+                    <li>• If you smell smoke stay low, smoke rises.</li>
+                    <li>• Use the nearest emergency exit.</li>
+                    <li>• Do not use the lift.</li>
+                    <li>• Go to the “Assembly Point” of the hotel.</li>
+                    <li>
+                      • Do not re-enter the building until told is safe to do
+                      so.
+                    </li>
+                    <li>
+                      • If you are forced to stay in your room, seal the door
+                      with wet towels, cover your mouth and nose with a damp
+                      cloth to ease breathing, open and stay near window until
+                      help arrives.
+                    </li>
+                  </ul>
+                </div>
+                {/*footer*/}
+                <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                  <button
+                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={() => setShowModal(false)}
+                  >
+                    Close
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+      ) : null}
+    </>
+  );
+};
+
+export default Modal;
